fe0a1554d48f90bd9805d4b3d6704d2fe9687beb
Ravi Prakash <rp2522407@gmail.com>
2024-07-03 21:13:07 +0530

    second commit with some changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://raviprakash:ravi1234@cluster0.hlxt7jp.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
    
 }
 
